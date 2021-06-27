8a05237c6be055638fe1d140135f361ab9aeda55
Changseo Jang <changseo.jang@riiid.co>
2021-06-27 17:56:50 +0900

    trying to implement check required extensions

--- main.cc
+++ main.cc
@@ -1,15 +1,18 @@
+#include <vulkan/vulkan_core.h>
+#include <algorithm>
 #include <cstdint>
+#include <string>
+#include <vector>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 class HelloTriangleApplication {
-public:
+ public:
   void run() {
     initWindow();
     initVulkan();
@@ -17,8 +20,68 @@ public:
     cleanup();
   }
 
-private:
+ private:
   GLFWwindow* window;
+  VkInstance instance;
+
+  bool extensionSupported(
+      const char* requiredExtensions[], int requiredExtensionCount,
+      std::vector<VkExtensionProperties> supportedExtensions) {
+    for (int i = 0; i < requiredExtensionCount; ++i) {
+      std::string requiredExtension = requiredExtensions[i];
+
+      bool exists = false;
+      for (auto property : supportedExtensions) {
+        if (property.extensionName == requiredExtension) {
+          exists = true;
+        }
+      }
+
+      if (!exists) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    uint32_t extensionCount = 0;
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount,
+                                           extensions.data());
+
+    if (!extensionSupported(glfwExtensions, glfwExtensionCount, extensions)) {
+      throw std::runtime_error("not supported!");
+    }
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+    if (result != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+  }
 
   void initWindow() {
     glfwInit();
@@ -26,13 +89,17 @@ private:
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
   }
-  void initVulkan() {}
+
+  void initVulkan() { createInstance(); }
+
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
       glfwPollEvents();
     }
   }
+
   void cleanup() {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
 
     glfwTerminate();
@@ -44,9 +111,9 @@ int main(void) {
 
   try {
     app.run();
-  } catch (const std::exception &e) {
+  } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
-	return EXIT_FAILURE;
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
